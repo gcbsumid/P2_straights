3925fdfb6451102e9e4c8b031f1a6d299cf20eaa
Didier Smith <didier.a.s.smith@gmail.com>
2012-07-12 14:05:32 -0400

    kill .o and .d files

--- gameplay/GamePlay.d
+++
@@ -1,2 +0,0 @@
-GamePlay.o: GamePlay.cpp GamePlay.h Player.h Card.h HumanPlayer.h \
- ComputerPlayer.h GameState.h ModelObserver.h ../gui/ViewInterface.h
--- gameplay/Player.d
+++
@@ -1,2 +0,0 @@
-Player.o: Player.cpp Player.h Card.h GamePlay.h HumanPlayer.h \
- ComputerPlayer.h GameState.h ../gui/ViewInterface.h
