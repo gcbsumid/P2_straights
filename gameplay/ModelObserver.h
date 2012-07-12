e5295bfd1c47d7d4e842bff484202dbdce91048f
Didier Smith <didier.a.s.smith@gmail.com>
2012-07-11 22:53:17 -0400

    add model observer

---
+++ gameplay/ModelObserver.h
@@ -0,0 +1,19 @@
+#ifndef MODEL_OBSERVER
+#define MODEL_OBSERVER
+
+// Defines the interface which the view implements and the model calls.
+
+class Card;
+
+class ModelObserver {
+    void Model_CardsDealt(std::vector<std::vector<Card*> >);
+    void Model_PlayerAdded(bool isHuman, int id);
+    void Model_ClearedCards();
+    void Model_CardDiscarded(int player, Card* card);
+    void Model_ScoreUpdated(int player, int score);
+    void Model_DiscardsCleared(int player);
+    void Model_CardPlayed(int player, Card*);
+    void Model_CardDiscarded(int player, Card*);
+};
+
+#endif
